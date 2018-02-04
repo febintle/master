56a00b48c417ddf414667f3f5e60f21bf04d27ca
febintle <yanyuhua1988@126.com>
2018-02-04 10:10:29 +0800

    index.php-1010-0958

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,15 @@
 <?php
 	$url = '../index.php?c=app&a=superadmin:index';
 	header("Location:{$url}");
+    /*
+    * 你好，我的朋友 02-04 10:02
+    abcdef and abcdef
+    mms s sdkldlk and
+    ifx_affected_rows dsd and apache_child_terminate
+    
+    if (end == x)
+        x = 10000;
+    else
+        x = 20000;
+    */
 	exit("<script>location.href='{$url}'</script>");
