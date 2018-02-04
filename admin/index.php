fa4de4ac8651d6ec83b919d409b292db1ae83811
febintle <yanyuhua1988@126.com>
2018-02-04 10:16:21 +0800

    master-index.php

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,12 @@
 <?php
 	$url = '../index.php?c=app&a=superadmin:index';
+    /*
+    I'M MASTER MY FRIDEND
+    HI NIHAO MA ?
+    WOHENHAO
+    IF X == 3000 AND X == 500
+        NI HAO
+        X = 5000000
+    */
 	header("Location:{$url}");
 	exit("<script>location.href='{$url}'</script>");
